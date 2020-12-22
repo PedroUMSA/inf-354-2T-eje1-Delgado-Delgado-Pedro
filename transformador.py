d2a4abb52acf1e2faa00a3c8e7ac706163c5f897
PedroUMSA <pdelgado3@umsa.bo>
2020-12-22 09:32:13 -0400

    Delgado Delgado Pedro

---
+++ transformador.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 22:25:53 2020
+
+@author: Roger
+"""
+
+import pandas as pd
+import numpy as np
+
+#Transformamos los datos de .arff a .csv
+path = ""
+f = open(path+"cpu.arff")
+lines = f.read().splitlines()
+header = []
+data = []
+types = []
+b = False
+for line in lines:
+    if("@attribute" in line):
+        atr = line.split(" ")
+        header.append(atr[1])
+        types.append(atr[2])
+    if(b):
+        dt = line.split(",")
+        data.append(dt)
+    if("@data" in line):
+        b = True
+    
+for dat in data:
+    for i in range(len(dat)):
+        if(types[i] == "numeric"):
+            dat[i] = int(dat[i])
+
+data = np.array(data)
+df = pd.DataFrame(data)
+df.columns = header
+df.to_csv(path+"cpu.csv", index = False, header = True)
+
+
+#leemos el archivo csv para verificar que se haya guardado satifacotriamnete
+
+csvFile = pd.read_csv(path+"cpu.csv")
+print(csvFile)
